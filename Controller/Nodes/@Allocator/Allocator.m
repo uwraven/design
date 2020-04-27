946179379a91475931072f673463fdb33e7b0759
Matt <flyinv1@gmail.com>
2020-04-27 10:56:55 -0700

    allocator mapping

--- Controller/Nodes/@Allocator/Allocator.m
+++ Controller/Nodes/@Allocator/Allocator.m
@@ -3,10 +3,13 @@ classdef Allocator < handle
 properties (Access = public)
     saturate = false
     limits
-    L_E
-    L_R1
-    L_R2
     clamped
+end
+
+properties (GetAccess = public, SetAccess = private)
+    L
+    H
+    Hc
 end
 
 methods (Access = public)
@@ -17,25 +20,28 @@ methods (Access = public)
         self.limits = reshape(limits, 6, 2);
     end
 
-    function self = setActuatorGeometry(self, l_e, lr1, lr2)
-        self.L_E = l_e;
-        self.L_R1 = lr1;
-        self.L_R2 = lr2;
+    function self = setActuatorArms(self, L)
+
+        self.L = L;
+
+        L1 = L(1);
+        L2 = L(2);
+        L3 = L(3);
+
+        self.H = [
+            1 0 0 -1 -1 0 0 1 1
+            0 1 0 0 0 -1 0 0 1
+            0 0 1 0 0 0 0 0 0 
+            0 -L1 0 0 0 -L2 0 0 L2
+            L1 0 0 L2 L2 0 0 -L2 -L2
+            0 0 0 -L3 L3 0 0 -L3 L3
+        ];
+
+        self.Hc = inv(self.H);
+
     end
 
-    function U = allocate(self, Ur)
-        % U is a vector [Fx Fy Fz Mx My Mz] with F in vehicle frame
-
-        % Solve nonlinear EOM
-        F_RY = (Ur(6) - Ur(2) * self.L_E) / (self.L_R1 - self.L_E);
-        F_RP = (Ur(5) - Ur(3) * self.L_E) / (self.L_R1 - self.L_E);
-        F_RR = 1 / (2 * self.L_R2) * Ur(4);
-
-        G = atan((F_RY - Ur(2)) /  Ur(1));
-        B = atan(((Ur(3) - F_RP) * cos(G)) / Ur(1));
-        F_E = Ur(1) / (cos(G) * cos(B));
-
-        U = [G B F_E F_RY F_RP F_RR];
+    function U = nonlinearAllocation(self, Ur)
 
     end
 
@@ -43,15 +49,7 @@ methods (Access = public)
         % U is a vector [Fx Fy Fz Mx My Mz] in body frame
 
         % Solve the linear EOM
-        U = [
-            Ur(1)
-            (Ur(6) - Ur(2) * self.L_R1) / (self.L_E - self.L_R1)
-            (Ur(5) - Ur(3) * self.L_R1) / (self.L_E - self.L_R1)
-            Ur(4) / (2 * self.L_R2)
-            (Ur(3) * self.L_E - Ur(5)) / (self.L_E - self.L_R1)
-            (Ur(2) * self.L_E - Ur(6)) / (self.L_E - self.L_R1)
-        ];
-        % [Fex Fey Fez Frr Frp Fry]
+        U = self.Hc * Ur;
 
         % Dumb actuator clamping for now
         % TODO: implement allocation strategy
